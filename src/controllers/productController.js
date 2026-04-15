1af579187eaec492f6ba8aa9a17d685b427a96a9
Matias <maty_2491@hotmail.com>
2026-04-15 19:50:55 -0300

    Mod productService y controller

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,7 +2,9 @@ import { createProductService } from "../services/productService.js"
 
 export const createProduct = async (req, res) => {
     try {
+        // SE RECIBE LA INFORMACION DEL CLIENTE
         const productData = req.body
+        // MANEJAMOS LA CREACION DEL PRODUCTO
         const savedProduct = await createProductService(productData)
         res.status(201).json(savedProduct)
     } catch (error) {
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,14 @@
 import Product from "../models/productModel.js"
 
 export const createProductService = async (productData) => {
+    // VALIDAR QUE EL PRODUCTO ES UNICO
+    const {name} = productData
+    const productExist = await Product.findOne({name})
+    if(productExist){
+        const error = new Error(`El producto ${name}, ya existe`)
+        error.statusCode = 400
+        throw error
+    }
     const newProduct = new Product(productData)
     const savedProduct = await newProduct.save()
     return savedProduct
